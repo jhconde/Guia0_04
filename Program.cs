f45d7f680f1473d53b566c704b12d2f3bcd9c8fe
JOSE HUMBERTO CONDE <81JC17856190@campus.economicas.uba.ar>
2024-04-08 19:06:04 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+{
+    // Solicitar al usuario que ingrese su nombre
+    Console.Write("Ingrese su nombre: ");
+    string nombre = Console.ReadLine();
+
+    // Presentar el mensaje con el nombre ingresado
+    Console.WriteLine("Hola " + nombre);
+
+    // Mantener la consola abierta hasta que el usuario presione una tecla
+    Console.ReadKey();
+}
+}
